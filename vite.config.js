4deb11a6457d2d4ca175b2755819c7e4d6fc7c34
khaled <khaledabdo7777@gmail.com>
2024-07-28 16:46:40 +0300

    upload all

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://kabdulrahmann.github.io/yalla-app/',
+  build: {
+    rollupOptions: {
+      // Add any needed external modules here
+    },
+  },
   plugins: [react()],
 })
